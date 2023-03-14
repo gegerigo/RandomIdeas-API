4488c80329a3cfcfa5a45f64e63cb86a42322344
gegerigo <gegerigo@gmail.com>
2023-03-14 12:12:34 +0100

    Refactored to use mongoDB with mongoose

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,6 +1,80 @@
 const express = require("express");
 const router = express.Router();
+const Idea = require("../models/Idea");
 
+// GET all ideas
+router.get("/", async (req, res) => {
+  try {
+    const ideas = await Idea.find();
+    res.json({ success: true, data: ideas });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ succes: false, error: "Something went wrong" });
+  }
+});
+
+// GET a single idea
+router.get("/:id", async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    res.json({ success: true, data: idea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ succes: false, error: "Something went wrong" });
+  }
+});
+
+// ADD an idea
+router.post("/", async (req, res) => {
+  const idea = new Idea({
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+  });
+
+  try {
+    const savedIdea = await idea.save();
+    res.json({ success: true, data: savedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ succes: false, error: "Something went wrong" });
+  }
+});
+
+// UPDATE a single idea
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          text: req.body.text,
+          tag: req.body.tag,
+        },
+      },
+      { new: true }
+    );
+    res.json({ success: true, data: updatedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ succes: false, error: "Something went wrong" });
+  }
+});
+
+// Delete a single idea
+router.delete("/:id", async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
+    res.json({ success: true, data: {} });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ succes: false, error: "Something went wrong" });
+  }
+});
+
+module.exports = router;
+
+/*
 const ideas = [
   {
     id: 1,
@@ -95,4 +169,4 @@ router.put("/:id", (req, res) => {
   res.json({ success: true, data: idea });
 });
 
-module.exports = router;
+*/
